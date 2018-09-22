4003e7143b1c003dbb04f41c6bec2a06a85b5208
Wanasak <u510610433@gmail.com>
2018-09-23 01:13:33 +0700

    deploy pwa

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = (props) => {
     const { id, name, email } = props;
     return (
         <div className="bg-light-green dib br3 pa3 ma2 grow">
-            <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
+            <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
             <div>
                 <h2>{ name }</h2>
                 <p>{ email }</p>
