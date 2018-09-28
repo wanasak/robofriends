4dd3b95edbf3f4f1d8699a238bec3f46b0b3fa47
Wanasak <u510610433@gmail.com>
2018-09-28 16:16:10 +0700

    snapshot testing + code coverage

--- src/components/Card.test.js
+++ src/components/Card.test.js
@@ -1,9 +1,7 @@
 import React from "react";
-import {
-    shallow
-} from "enzyme";
+import { shallow } from "enzyme";
 import Card from "./Card";
 
 it("expect to render Card component", () => {
-    expect(shallow( < Card / > ).length).toEqual(1);
-})
+  expect(shallow(<Card />)).toMatchSnapshot();
+});
---
+++ src/components/CardList.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { shallow } from "enzyme";
+import CardList from "./CardList";
+
+it("expect to render CardList component", () => {
+    const mockRobots = [{
+        id: 1,
+        name: "John Snow",
+        username: "JohnJohn",
+        email: "john@gmail.com"
+    }];
+  expect(shallow(<CardList robots={mockRobots} />)).toMatchSnapshot();
+});
